3024cfe8029fa49a78bbd982654c7bf1ffe98899
azure123devops123 <azure123.devops123@gmail.com>
2024-02-22 04:38:20 +1100

    Updated

--- src/main/java/com/devops/springfirstapp/HelloController.java
+++ src/main/java/com/devops/springfirstapp/HelloController.java
@@ -1,16 +1,16 @@
 package com.devops.springfirstapp;  
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;  
 import org.springframework.web.bind.annotation.RestController;
-import com.devops.springfirstapp.environment.InstanceInformationService;
+// import com.devops.springfirstapp.environment.InstanceInformationService;
 
 @RestController  
 public class HelloController   {
-    @Autowired
-	private InstanceInformationService service;
+    // @Autowired
+	// private InstanceInformationService service;
 
     @RequestMapping("/hello")  
     public String hello()   {  
-    return "Hello World Greetings from Spring Boot Application" + " V3 " + service.retrieveInstanceInfo();
+    return "Hello World Greetings from Spring Boot Application" + " VERSION NO. 3 ";
     }
 }
--- src/main/java/com/devops/springfirstapp/environment/InstanceInformationService.java
+++ src/main/java/com/devops/springfirstapp/environment/InstanceInformationService.java
@@ -1,20 +1,20 @@
-package com.devops.springfirstapp.environment;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+// package com.devops.springfirstapp.environment;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.stereotype.Service;
 
-@Service
-public class InstanceInformationService {
+// @Service
+// public class InstanceInformationService {
 
-	private static final String HOST_NAME = "HOSTNAME";
+// 	private static final String HOST_NAME = "HOSTNAME";
 
-	private static final String DEFAULT_ENV_INSTANCE_GUID = "LOCAL";
+// 	private static final String DEFAULT_ENV_INSTANCE_GUID = "LOCAL";
 
-	// @Value(${ENVIRONMENT_VARIABLE_NAME:DEFAULT_VALUE})
-	@Value("${" + HOST_NAME + ":" + DEFAULT_ENV_INSTANCE_GUID + "}")
-	private String hostName;
+// 	// @Value(${ENVIRONMENT_VARIABLE_NAME:DEFAULT_VALUE})
+// 	@Value("${" + HOST_NAME + ":" + DEFAULT_ENV_INSTANCE_GUID + "}")
+// 	private String hostName;
 
-	public String retrieveInstanceInfo() {
-		return hostName.substring(hostName.length()-5);
-	}
+// 	public String retrieveInstanceInfo() {
+// 		return hostName.substring(hostName.length()-5);
+// 	}
 
-}
+// }
