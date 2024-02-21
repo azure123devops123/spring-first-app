df636fbdc4cea5cbf723c74ddaf98d1bd03969b2
azure123devops123 <azure123.devops123@gmail.com>
2024-02-22 04:17:41 +1100

    Updated

--- src/main/java/com/devops/springfirstapp/HelloController.java
+++ src/main/java/com/devops/springfirstapp/HelloController.java
@@ -14,18 +14,3 @@ public class HelloController   {
     return "Hello World Greetings from Spring Boot Application" + " V3 " + service.retrieveInstanceInfo();
     }
 }
-
-// OLD VERSION
-
-// package com.devops.springfirstapp;  
-// import org.springframework.web.bind.annotation.RequestMapping;  
-// import org.springframework.web.bind.annotation.RestController;  
-// @RestController  
-// public class HelloController   
-// {  
-// @RequestMapping("/hello")  
-// public String hello()   
-// {  
-// return "Greetings from Spring Boot Successful - Excellent JOB VERSION 3";
-// }
-// }
